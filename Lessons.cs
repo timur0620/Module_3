133a8cff3a01e7f794c16c8bc5770445ca6792e5
Timur <timur0620@list.ru>
2024-01-29 21:27:46 +0300

    Добавьте файлы проекта.

---
+++ Lessons.cs
@@ -0,0 +1,72 @@
+﻿namespace Module_3
+{
+    class Lessons
+    {
+        static void Work(string[] args)
+        {
+            Random rand = new Random();
+            int num = rand.Next(10, 100);
+            double num_2 = rand.NextDouble() * 100;
+
+            int result = (num > num_2) ? 0 : 1;
+            Console.WriteLine((num > num_2) ? 0 : 1);
+
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+            }
+            for (int i = 1; i < 22; i += 2)
+            {
+                //Console.WriteLine(i);
+            }
+            for (double step = -1; step <= 0; step += 0.25f)
+            {
+                Console.WriteLine(step);
+            }
+
+            Console.WriteLine("Enter number;");
+            int number = int.Parse(Console.ReadLine());
+
+            bool flag = false;
+            while (flag)
+            {
+                switch (number)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("You number = 1");
+                            number = 2;
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("You number = 2");
+                            number = 3;
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("You number = 3");
+                            number = 4;
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("You number = 4");
+                            flag = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("You number = ???");
+                            flag = false;
+                            break;
+                        }
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+
+namespace Module_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_1();
+            Task_2();
+            Task_3();
+            Task_4();
+            Task_5();
+        }
+        static void Task_1()
+        {
+            Console.WriteLine("Enter number;");
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0) Console.WriteLine("Even");
+            else Console.WriteLine("Not even");
+        }
+        static void Task_2()
+        {
+            Console.WriteLine("How much is the card in you hands?;");
+            int iterNumber = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            for (int i = 0; i < iterNumber; i++)
+            {
+                Console.WriteLine("Enter cards ");
+                string cardNumber = Console.ReadLine().ToUpper();   
+
+                if (cardNumber.Equals("Q") || cardNumber.Equals("J") || cardNumber.Equals("T") || cardNumber.Equals("K"))
+                {
+                    result += 10;
+                    continue;
+                }
+                int currentNumber = int.Parse(cardNumber);
+                result += currentNumber;
+            }
+            Console.WriteLine($"Sum card = {result}");
+        }
+        static void Task_3()
+        {
+            Console.WriteLine("Enter Number ");
+            int number = int.Parse(Console.ReadLine());
+
+            List<int> listPrimeNumbers = new List<int>();
+
+            List<int> listCompositeNumbers = new List<int>();
+
+            int count = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j != 0)
+                    {
+                        count += 1;
+                    }
+                }
+                if (count == (i - 2)) listPrimeNumbers.Add(i);
+                else listCompositeNumbers.Add(i);
+
+                count = 0;
+            }
+            Console.WriteLine("PrimeNumbers " + string.Join("\t", listPrimeNumbers));
+            Console.WriteLine("CompositeNumbers " + string.Join("\t", listCompositeNumbers));
+        }
+        static void Task_4()
+        {
+            Console.WriteLine("Enter the length;");
+            int iterNumber = int.Parse(Console.ReadLine());
+
+            int minNumber = int.MaxValue;
+
+            for (int i = 0; i < iterNumber; i++)
+            {
+                Console.WriteLine("Enter number");
+                int Number = int.Parse(Console.ReadLine());
+
+                if (minNumber > Number) minNumber = Number; 
+            }
+            Console.WriteLine($"Min number  {minNumber}");
+        }
+        static void Task_5()
+        {
+            Console.WriteLine("Enter max values;");
+            int iterNumber = int.Parse(Console.ReadLine());
+
+            int secretNum = new Random().Next(0, iterNumber);
+            
+            while(true)
+            {
+                Console.WriteLine("Enter variant;");
+                int variantNumber = int.Parse(Console.ReadLine());
+
+                if (variantNumber > secretNum)
+                {
+                    Console.WriteLine("-");
+                }
+                else if (variantNumber < secretNum)
+                {
+                    Console.WriteLine("+");
+                }
+                else
+                {
+                    Console.WriteLine($"Done!!! Secret number {secretNum}");
+                    break;
+                }
+            }
+        }
+    }
+}
